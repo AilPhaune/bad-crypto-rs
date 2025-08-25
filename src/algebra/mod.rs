445f9f3b7728ae1d879650a792f2b2792da311b5
AilPhaune <109510657+AilPhaune@users.noreply.github.com>
2025-08-25 02:31:09 +0200

    algebraic structures

---
+++ src/algebra/mod.rs
@@ -0,0 +1,423 @@
+use std::{fmt::Debug, marker::PhantomData};
+
+use rug::{Integer, integer::IsPrime};
+
+pub mod modular;
+
+pub trait Group<T: Debug>: Debug {
+    /// The additive identity element `0`
+    fn additive_identity(&self) -> T;
+
+    /// Checks if `x` is the additive identity
+    fn is_additive_identity(&self, x: &T) -> bool;
+
+    /// The additive inverse of `x`
+    fn additive_inverse(&self, x: &T) -> T;
+
+    /// Addition of `x` and `y`
+    fn add(&self, x: &T, y: &T) -> T;
+
+    /// Subtraction of `x` and `y`
+    fn sub(&self, x: &T, y: &T) -> T {
+        self.add(x, &self.additive_inverse(y))
+    }
+
+    /// Equality of `x` and `y`
+    fn eq(&self, x: &T, y: &T) -> bool;
+
+    /// Clones `x`
+    fn clone(&self, x: &T) -> T;
+
+    /// Repeated addition of `x` `n` times
+    fn repeated_addition(&self, x: T, n: &Integer) -> T {
+        if n.is_zero() {
+            return self.additive_identity();
+        } else if *n < 0 {
+            return self.repeated_addition(self.additive_inverse(&x), &n.as_neg());
+        }
+
+        let mut result = self.additive_identity();
+
+        for i in (0..n.significant_bits()).rev() {
+            result = self.add(&result, &result);
+            if n.get_bit(i) {
+                result = self.add(&result, &x);
+            }
+        }
+
+        result
+    }
+}
+
+pub trait Ring<T: Debug>: Group<T> {
+    /// The multiplicative identity element `1`
+    fn multiplicative_identity(&self) -> T;
+
+    /// The multiplicative inverse of `x`
+    fn multiplicative_inverse(&self, x: &T) -> Option<T>;
+
+    /// Multiplication of `x` and `y`
+    fn mul(&self, x: &T, y: &T) -> T;
+}
+
+pub trait Field<T: Debug, NZ: Debug>: Ring<T> {
+    /// Constructs an instance of a non zero field element
+    fn construct_non_zero(&self, x: T) -> Option<NZ>;
+
+    /// Deconstructs a non zero field element
+    fn deconstruct_non_zero(&self, x: NZ) -> T;
+
+    /// Constructs an instance of a non zero field element
+    fn get_non_zero(&self, x: &T) -> Option<NZ>;
+
+    /// Deconstructs a non zero field element
+    fn get_element_from_non_zero(&self, x: &NZ) -> T;
+
+    /// Division of `x` and `y`
+    fn div(&self, x: &T, y: &NZ) -> T;
+}
+
+pub trait FiniteField<T: Debug, NZ: Debug, U: Debug>: Field<T, NZ> {
+    /// The number of elements of the field
+    fn order(&self) -> U;
+
+    /// The field's characteristic
+    fn characteristic(&self) -> Integer;
+}
+
+pub trait HasAdditiveIdentity: Debug {
+    fn additive_identity() -> Self;
+}
+
+pub trait HasMultiplicativeIdentity: Debug {
+    fn multiplicative_identity() -> Self;
+}
+
+pub trait CheckIsPrime: HasAdditiveIdentity + HasMultiplicativeIdentity {
+    fn is_prime(&self) -> IsPrime;
+}
+
+macro_rules! is_prime_impl {
+    ($t: ty) => {
+        impl HasAdditiveIdentity for $t {
+            fn additive_identity() -> Self {
+                0
+            }
+        }
+
+        impl HasMultiplicativeIdentity for $t {
+            fn multiplicative_identity() -> Self {
+                1
+            }
+        }
+
+        impl CheckIsPrime for $t {
+            fn is_prime(&self) -> IsPrime {
+                let i: Integer = (*self).into();
+                i.is_probably_prime(100)
+            }
+        }
+    };
+}
+
+is_prime_impl! { u8 }
+is_prime_impl! { u16 }
+is_prime_impl! { u32 }
+is_prime_impl! { u64 }
+is_prime_impl! { u128 }
+
+is_prime_impl! { i8 }
+is_prime_impl! { i16 }
+is_prime_impl! { i32 }
+is_prime_impl! { i64 }
+is_prime_impl! { i128 }
+
+is_prime_impl! { usize }
+is_prime_impl! { isize }
+
+impl HasAdditiveIdentity for Integer {
+    fn additive_identity() -> Self {
+        0.into()
+    }
+}
+
+impl HasMultiplicativeIdentity for Integer {
+    fn multiplicative_identity() -> Self {
+        1.into()
+    }
+}
+
+impl CheckIsPrime for Integer {
+    fn is_prime(&self) -> IsPrime {
+        self.is_probably_prime(100)
+    }
+}
+
+#[derive(Debug)]
+pub struct Fraction<T: Debug, NZ: Debug, U: Debug, F: FiniteField<T, NZ, U>> {
+    _field: PhantomData<(T, NZ, U, F)>,
+    num: T,
+    den: T,
+}
+
+impl<T: Debug, NZ: Debug, U: Debug, F: FiniteField<T, NZ, U>> Fraction<T, NZ, U, F> {
+    pub fn new(num: T, den: T) -> Fraction<T, NZ, U, F> {
+        Fraction {
+            _field: PhantomData,
+            num,
+            den,
+        }
+    }
+
+    pub fn is_zero(&self, field: &F) -> bool {
+        field.is_additive_identity(&self.num)
+    }
+
+    pub fn mul(&self, other: &Fraction<T, NZ, U, F>, field: &F) -> Fraction<T, NZ, U, F> {
+        Fraction::new(
+            field.mul(&self.num, &other.num),
+            field.mul(&self.den, &other.den),
+        )
+    }
+
+    pub fn div(&self, other: &Fraction<T, NZ, U, F>, field: &F) -> Fraction<T, NZ, U, F> {
+        Fraction::new(
+            field.mul(&self.num, &other.den),
+            field.mul(&self.den, &other.num),
+        )
+    }
+
+    pub fn inv(&self, field: &F) -> Fraction<T, NZ, U, F> {
+        Fraction::new(field.clone(&self.den), field.clone(&self.num))
+    }
+
+    pub fn invert(&mut self) {
+        std::mem::swap(&mut self.num, &mut self.den);
+    }
+
+    pub fn get_num(&self) -> &T {
+        &self.num
+    }
+
+    pub fn get_den(&self) -> &T {
+        &self.den
+    }
+
+    pub fn add(&self, other: &Fraction<T, NZ, U, F>, field: &F) -> Fraction<T, NZ, U, F> {
+        // a/b + c/d = ad/bd + bc/ab = (ad + bc)/(bd)
+        let ad = field.mul(&self.num, &other.den);
+        let bc = field.mul(&self.den, &other.num);
+        let bd = field.mul(&self.den, &other.den);
+        Fraction::new(field.add(&ad, &bc), bd)
+    }
+
+    pub fn neg(&self, field: &F) -> Fraction<T, NZ, U, F> {
+        Fraction::new(field.additive_inverse(&self.num), field.clone(&self.den))
+    }
+
+    pub fn negate(&mut self, field: &F) {
+        self.num = field.additive_inverse(&self.num);
+    }
+
+    pub fn sub(&self, other: &Fraction<T, NZ, U, F>, field: &F) -> Fraction<T, NZ, U, F> {
+        // a/b - c/d = ad/bd - bc/ab = (ad - bc)/(bd)
+        let ad = field.mul(&self.num, &other.den);
+        let bc = field.mul(&self.den, &other.num);
+        let bd = field.mul(&self.den, &other.den);
+        Fraction::new(field.sub(&ad, &bc), bd)
+    }
+
+    pub fn to_field_element(&self, field: &F) -> Option<T> {
+        let nz = field.get_non_zero(&self.den)?;
+        Some(field.div(&self.num, &nz))
+    }
+}
+
+impl Group<bool> for bool {
+    fn additive_identity(&self) -> bool {
+        false
+    }
+
+    fn add(&self, x: &bool, y: &bool) -> bool {
+        x ^ y
+    }
+
+    fn additive_inverse(&self, x: &bool) -> bool {
+        *x
+    }
+
+    fn sub(&self, x: &bool, y: &bool) -> bool {
+        x ^ y
+    }
+
+    fn clone(&self, x: &bool) -> bool {
+        *x
+    }
+
+    fn eq(&self, x: &bool, y: &bool) -> bool {
+        x == y
+    }
+
+    fn is_additive_identity(&self, x: &bool) -> bool {
+        !x
+    }
+
+    fn repeated_addition(&self, x: bool, n: &Integer) -> bool {
+        if x { n.get_bit(0) } else { false }
+    }
+}
+
+impl Ring<bool> for bool {
+    fn mul(&self, x: &bool, y: &bool) -> bool {
+        x & y
+    }
+
+    fn multiplicative_identity(&self) -> bool {
+        true
+    }
+
+    fn multiplicative_inverse(&self, x: &bool) -> Option<bool> {
+        if *x { Some(true) } else { None }
+    }
+}
+
+// Because bool is isomorphic to Z/2Z, which is a field because 2 is prime
+impl Field<bool, ()> for bool {
+    fn div(&self, x: &bool, _: &()) -> bool {
+        // non zero bool is 1, divide by 1 is identity
+        *x
+    }
+
+    fn construct_non_zero(&self, x: bool) -> Option<()> {
+        if x { Some(()) } else { None }
+    }
+
+    fn deconstruct_non_zero(&self, _: ()) -> bool {
+        true
+    }
+
+    fn get_non_zero(&self, x: &bool) -> Option<()> {
+        if *x { Some(()) } else { None }
+    }
+
+    fn get_element_from_non_zero(&self, _: &()) -> bool {
+        true
+    }
+}
+
+impl FiniteField<bool, (), usize> for bool {
+    fn characteristic(&self) -> Integer {
+        // characteristic of Z/p^nZ is p
+        2.into()
+    }
+
+    fn order(&self) -> usize {
+        2
+    }
+}
+
+macro_rules! mod2n_inv {
+    ($x:expr, $n:expr, $T:ty) => {{
+        let x: $T = $x;
+        let n: u32 = $n;
+
+        if x % 2 == 0 {
+            None
+        } else {
+            let mut y: $T = 1;
+            let mut m: $T = 2;
+
+            while m < (1 << n) {
+                y = y.wrapping_mul((2 as$T).wrapping_sub(x.wrapping_mul(y))) % (m << 1);
+                m <<= 1;
+            }
+
+            Some(y & ((1 as $T).wrapping_shl(n) - 1))
+        }
+    }};
+}
+
+macro_rules! group_impl_native_type {
+    ($t: ident) => {
+        impl Group<$t> for $t {
+            fn additive_identity(&self) -> $t {
+                0
+            }
+
+            fn add(&self, x: &$t, y: &$t) -> $t {
+                x.wrapping_add(*y)
+            }
+
+            fn additive_inverse(&self, x: &$t) -> $t {
+                x.wrapping_neg()
+            }
+
+            fn sub(&self, x: &$t, y: &$t) -> $t {
+                x.wrapping_sub(*y)
+            }
+
+            fn clone(&self, x: &$t) -> $t {
+                *x
+            }
+
+            fn eq(&self, x: &$t, y: &$t) -> bool {
+                x == y
+            }
+
+            fn is_additive_identity(&self, x: &$t) -> bool {
+                *x == 0
+            }
+        }
+    };
+}
+
+macro_rules! ring_impl_native_type {
+    ($t: ident, $numbits: expr) => {
+        impl Ring<$t> for $t {
+            fn mul(&self, x: &$t, y: &$t) -> $t {
+                x.wrapping_mul(*y)
+            }
+
+            fn multiplicative_identity(&self) -> $t {
+                1
+            }
+
+            fn multiplicative_inverse(&self, x: &$t) -> Option<$t> {
+                mod2n_inv!(*x, $numbits, $t)
+            }
+        }
+    };
+}
+
+group_impl_native_type!(i8);
+group_impl_native_type!(u8);
+group_impl_native_type!(i16);
+group_impl_native_type!(u16);
+group_impl_native_type!(i32);
+group_impl_native_type!(u32);
+group_impl_native_type!(i64);
+group_impl_native_type!(u64);
+group_impl_native_type!(i128);
+group_impl_native_type!(u128);
+group_impl_native_type!(isize);
+group_impl_native_type!(usize);
+
+ring_impl_native_type!(u8, 8);
+ring_impl_native_type!(u16, 16);
+ring_impl_native_type!(u32, 32);
+ring_impl_native_type!(u64, 64);
+ring_impl_native_type!(u128, 128);
+
+#[cfg(test)]
+mod tests {
+    use crate::algebra::Group;
+
+    #[test]
+    fn test_repeated_addition() {
+        for m in 0..1000 {
+            for n in 0..1000 {
+                assert_eq!(0.repeated_addition(m, &n.into()), m * n);
+            }
+        }
+    }
+}
---
+++ src/algebra/modular.rs
@@ -0,0 +1,257 @@
+use std::fmt::Debug;
+
+use rug::{Complete, Integer, integer::IsPrime, ops::RemRounding};
+
+use crate::{
+    Checked, Unchecked,
+    algebra::{CheckIsPrime, Field, FiniteField, Group, Ring},
+};
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct PrimeField {
+    modulus: Integer,
+}
+
+impl PrimeField {
+    /// # Safety
+    /// If modulus is not prime, Z/nZ will not be a field
+    pub unsafe fn new_unchecked(modulus: Integer) -> Unchecked<PrimeField> {
+        Unchecked::new(PrimeField { modulus })
+    }
+
+    pub fn new(modulus: Integer) -> Option<Checked<PrimeField>> {
+        match modulus.is_prime() {
+            IsPrime::No => None,
+            IsPrime::Probably => Some(Checked::Unchecked(Unchecked::new(PrimeField { modulus }))),
+            IsPrime::Yes => Some(Checked::Checked(PrimeField { modulus })),
+        }
+    }
+
+    pub fn modulus(&self) -> &Integer {
+        &self.modulus
+    }
+
+    pub fn norm(&self, z: &Integer) -> Integer {
+        z.rem_euc(&self.modulus).complete()
+    }
+
+    pub fn multiplicative_group(self) -> CyclicPrimeMultiplicativeGroup {
+        CyclicPrimeMultiplicativeGroup {
+            modulus: self.modulus,
+        }
+    }
+
+    pub fn temp_multiplicative_group<'a>(&'a self) -> TempCyclicPrimeMultiplicativeGroup<'a> {
+        TempCyclicPrimeMultiplicativeGroup {
+            modulus: &self.modulus,
+        }
+    }
+}
+
+impl Group<Integer> for PrimeField {
+    fn additive_identity(&self) -> Integer {
+        0.into()
+    }
+
+    fn is_additive_identity(&self, x: &Integer) -> bool {
+        self.norm(x) == 0
+    }
+
+    fn additive_inverse(&self, x: &Integer) -> Integer {
+        self.norm(&x.as_neg())
+    }
+
+    fn add(&self, x: &Integer, y: &Integer) -> Integer {
+        let z = (x + y).complete();
+        self.norm(&z)
+    }
+
+    fn clone(&self, x: &Integer) -> Integer {
+        self.norm(x)
+    }
+
+    fn sub(&self, x: &Integer, y: &Integer) -> Integer {
+        let z = (x - y).complete();
+        self.norm(&z)
+    }
+
+    fn eq(&self, x: &Integer, y: &Integer) -> bool {
+        self.norm(x) == self.norm(y)
+    }
+
+    fn repeated_addition(&self, x: Integer, n: &Integer) -> Integer {
+        self.norm(&(x * n))
+    }
+}
+
+impl Ring<Integer> for PrimeField {
+    fn multiplicative_identity(&self) -> Integer {
+        1.into()
+    }
+
+    fn mul(&self, x: &Integer, y: &Integer) -> Integer {
+        let z = (x * y).complete();
+        self.norm(&z)
+    }
+
+    fn multiplicative_inverse(&self, x: &Integer) -> Option<Integer> {
+        x.clone().invert(&self.modulus).ok()
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct PrimeFieldNonZeroInteger(Integer);
+
+impl PrimeFieldNonZeroInteger {
+    pub fn get_ref(&self) -> &Integer {
+        &self.0
+    }
+
+    pub fn get(self) -> Integer {
+        self.0
+    }
+}
+
+impl Field<Integer, PrimeFieldNonZeroInteger> for PrimeField {
+    fn construct_non_zero(&self, x: Integer) -> Option<PrimeFieldNonZeroInteger> {
+        let x = self.norm(&x);
+        if x.is_zero() {
+            None
+        } else {
+            Some(PrimeFieldNonZeroInteger(x))
+        }
+    }
+
+    fn deconstruct_non_zero(&self, x: PrimeFieldNonZeroInteger) -> Integer {
+        x.0
+    }
+
+    fn get_element_from_non_zero(&self, x: &PrimeFieldNonZeroInteger) -> Integer {
+        x.0.clone()
+    }
+
+    fn get_non_zero(&self, x: &Integer) -> Option<PrimeFieldNonZeroInteger> {
+        let x = self.norm(x);
+        if x.is_zero() {
+            None
+        } else {
+            Some(PrimeFieldNonZeroInteger(x))
+        }
+    }
+
+    fn div(&self, x: &Integer, y: &PrimeFieldNonZeroInteger) -> Integer {
+        self.mul(x, &self.multiplicative_inverse(&y.0).unwrap())
+    }
+}
+
+impl FiniteField<Integer, PrimeFieldNonZeroInteger, Integer> for PrimeField {
+    fn order(&self) -> Integer {
+        self.modulus.clone()
+    }
+
+    fn characteristic(&self) -> Integer {
+        self.modulus.clone()
+    }
+}
+
+#[derive(Debug)]
+pub struct CyclicPrimeMultiplicativeGroup {
+    modulus: Integer,
+}
+
+#[derive(Debug)]
+pub struct TempCyclicPrimeMultiplicativeGroup<'a> {
+    modulus: &'a Integer,
+}
+
+impl CyclicPrimeMultiplicativeGroup {
+    /// # Safety
+    /// If modulus is not prime, Z/nZ will not have a multiplicative group
+    pub unsafe fn new_unchecked(modulus: Integer) -> Unchecked<CyclicPrimeMultiplicativeGroup> {
+        Unchecked::new(CyclicPrimeMultiplicativeGroup { modulus })
+    }
+
+    pub fn new(modulus: Integer) -> Option<Checked<CyclicPrimeMultiplicativeGroup>> {
+        match modulus.is_prime() {
+            IsPrime::No => None,
+            IsPrime::Probably => Some(Checked::Unchecked(Unchecked::new(
+                CyclicPrimeMultiplicativeGroup { modulus },
+            ))),
+            IsPrime::Yes => Some(Checked::Checked(CyclicPrimeMultiplicativeGroup { modulus })),
+        }
+    }
+
+    pub fn modulus(&self) -> &Integer {
+        &self.modulus
+    }
+
+    pub fn norm(&self, z: &Integer) -> Integer {
+        z.rem_euc(&self.modulus).complete()
+    }
+}
+
+impl<'a> TempCyclicPrimeMultiplicativeGroup<'a> {
+    pub fn modulus(&self) -> &Integer {
+        &self.modulus
+    }
+
+    pub fn norm(&self, z: &Integer) -> Integer {
+        z.rem_euc(self.modulus).complete()
+    }
+}
+
+// The group is (Z/pZ \ {0}, *)
+impl Group<Integer> for CyclicPrimeMultiplicativeGroup {
+    fn additive_identity(&self) -> Integer {
+        1.into()
+    }
+
+    fn add(&self, x: &Integer, y: &Integer) -> Integer {
+        let z = (x * y).complete();
+        self.norm(&z)
+    }
+
+    fn additive_inverse(&self, x: &Integer) -> Integer {
+        x.clone().invert(&self.modulus).unwrap()
+    }
+
+    fn clone(&self, x: &Integer) -> Integer {
+        self.norm(x)
+    }
+
+    fn eq(&self, x: &Integer, y: &Integer) -> bool {
+        self.norm(x) == self.norm(y)
+    }
+
+    fn is_additive_identity(&self, x: &Integer) -> bool {
+        self.norm(x) == 1
+    }
+}
+
+// The group is (Z/pZ \ {0}, *)
+impl<'a> Group<Integer> for TempCyclicPrimeMultiplicativeGroup<'a> {
+    fn additive_identity(&self) -> Integer {
+        1.into()
+    }
+
+    fn add(&self, x: &Integer, y: &Integer) -> Integer {
+        let z = (x * y).complete();
+        self.norm(&z)
+    }
+
+    fn additive_inverse(&self, x: &Integer) -> Integer {
+        x.clone().invert(&self.modulus).unwrap()
+    }
+
+    fn clone(&self, x: &Integer) -> Integer {
+        self.norm(x)
+    }
+
+    fn eq(&self, x: &Integer, y: &Integer) -> bool {
+        self.norm(x) == self.norm(y)
+    }
+
+    fn is_additive_identity(&self, x: &Integer) -> bool {
+        self.norm(x) == 1
+    }
+}
